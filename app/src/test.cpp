1cc9c043e153a341800cef8001577d560f8e0b4f
hnen <hhatinen@gmail.com>
2018-01-20 00:50:59 +0200

    initial simple app

---
+++ app/src/test.cpp
@@ -0,0 +1,17 @@
+
+#include "GLFW/glfw3.h"
+#include <iostream>
+
+using std::cout;
+
+int main() {
+
+    if (glfwInit()) {      
+        
+    }
+
+    cout << "O HAI" << std::endl;
+
+    return 0;
+}
+
